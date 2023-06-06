5fb203924b330edd87250dfb82901bf85cbcdcdf
Syed Faraz <syedfarazuddin73@gmail.com>
2023-06-06 08:34:55 +0530

    completed front end of the website

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ window.addEventListener("scroll", () => {
   }
 });
 
-const alternateStyles = document.querySelectorAll(".alternative_style");
+const alternateStyles = document.querySelectorAll(".alternate_style");
 
 function setActiveStyle(color) {
   alternateStyles.forEach((style) => {
@@ -21,3 +21,17 @@ function setActiveStyle(color) {
     }
   });
 }
+
+const dayNight = document.querySelector(".day_night");
+dayNight.addEventListener("click", () => {
+  dayNight.querySelector("i").classList.add("fa-sun");
+  dayNight.querySelector("i").classList.toggle("fa-moon");
+  document.body.classList.toggle("dark");
+});
+window.addEventListener("load", () => {
+  if (document.body.classList.contains("dark")) {
+    dayNight.querySelector("i").classList.add("fa-sun");
+  } else {
+    dayNight.querySelector("i").classList.add("fa-moon");
+  }
+});
